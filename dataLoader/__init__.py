d3a44c149d3ec523597734396269f7092a16fe4f
xharlie <charlie.xu007@yahoo.com>
2022-08-30 18:58:42 -0700

    more fix

--- dataLoader/__init__.py
+++ dataLoader/__init__.py
@@ -1,13 +1,20 @@
 from .llff import LLFFDataset
 from .blender import BlenderDataset
+from .blender import BlenderMVSDataset
 from .nsvf import NSVF
 from .tankstemple import TanksTempleDataset
+from .tankstempleBG import TanksTempleDatasetBG
 from .your_own_data import YourOwnDataset
+from .scannet import ScannetDataset
 
 
 
 dataset_dict = {'blender': BlenderDataset,
                'llff':LLFFDataset,
                'tankstemple':TanksTempleDataset,
+               'TanksAndTempleBG':TanksTempleDatasetBG,
                'nsvf':NSVF,
+               'scannet':ScannetDataset,
                 'own_data':YourOwnDataset}
+
+mvs_dataset_dict = {'blender': BlenderMVSDataset}
